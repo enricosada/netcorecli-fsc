c61162098c3aff92667af2b43d8d57a142a60e27
Enrico Sada <enrico@sada.io>
2016-11-16 18:48:10 +0100

    add web

---
+++ examples/preview3/web/Controllers/HomeController.fs
@@ -0,0 +1,24 @@
+﻿namespace MvcApp.Controllers
+
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Mvc
+
+type HomeController () =
+    inherit Controller()
+
+    member this.Index () =
+        this.View()
+
+    member this.About () =
+        ViewData.["Message"] <- "Your application description page."
+        this.View()
+
+    member this.Contact () =
+        ViewData.["Message"] <- "Your contact page."
+        this.View()
+
+    member this.IActionResult Error () =
+        this.View();
---
+++ examples/preview3/web/Program.fs
@@ -0,0 +1,21 @@
+﻿namespace MvcApp
+
+open System
+open System.Collections.Generic
+open System.IO
+open System.Linq
+open System.Threading.Tasks
+open Microsoft.AspNetCore.Hosting
+
+module Program =
+
+    [<EntryPoint>]
+    let main args =
+        var host = new WebHostBuilder()
+            .UseKestrel()
+            .UseContentRoot(Directory.GetCurrentDirectory())
+            .UseIISIntegration()
+            .UseStartup<Startup>()
+            .Build()
+
+        host.Run()
