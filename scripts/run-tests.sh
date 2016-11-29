16fe69ce44dd82f4430adcd0d08ff4b6e3fa91db
Enrico Sada <enrico@sada.io>
2016-11-29 19:32:40 +0100

    f

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -1,12 +1,6 @@
 
-SOURCE="${BASH_SOURCE[0]}"
-while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-  SOURCE="$(readlink "$SOURCE")"
-  [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
-done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
-REPOROOT="$(dirname $DIR)/rootdir"
+BASEDIR=$(dirname "$0")
+REPOROOT=$(dirname "$BASEDIR")
 
 # test helper
 
