ed9b8e16aabd5fc0187c5482bcb90bf1b314d91f
Enrico Sada <enrico@sada.io>
2016-11-30 19:08:11 +0100

    fix script

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -21,16 +21,16 @@ RunCmd ()
 
 cd "$REPOROOT/src/dotnet-compile-fsc"
 
-Run-Cmd "dotnet" 'restore -v Information --no-cache --configfile "$REPOROOT\test\NuGet.Config"' 
+RunCmd "dotnet" 'restore -v Information --no-cache --configfile "$REPOROOT\test\NuGet.Config"' 
 
-Run-Cmd "dotnet" "-v pack -C Release"
+RunCmd "dotnet" "-v pack -C Release"
 
 # run tests
 
 cd "$REPOROOT/test/"
 
-Run-Cmd "dotnet" "restore -v Information --no-cache --configfile `"$REPOROOT/test/NuGet.Config`"" 
+RunCmd "dotnet" "restore -v Information --no-cache --configfile `"$REPOROOT/test/NuGet.Config`"" 
 
 cd "$REPOROOT/test/dotnet-new.Tests"
 
-Run-Cmd "dotnet" "-v test"
+RunCmd "dotnet" "-v test"
